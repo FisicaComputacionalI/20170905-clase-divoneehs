72689512b36dfb44f87a8065d3799dc941101ba6
Diana Ivonee Huitzil Sosa <31254618+divoneehs@users.noreply.github.com>
2017-09-05 12:50:24 -0500

    Update dosgraficas.py

--- dosgraficas.py
+++ dosgraficas.py
@@ -2,10 +2,10 @@ import numpy as np
 import pylab as pl
 x = [5000, 6000, 7000, 8000, 9000]
 y = [65, 78, 88, 89, 93]
-pl.plot(x, y)
+pl.plot(x, y, 'D' )
 x1 = [7000, 8000, 9000, 10000, 11000]
 y2 = [65, 75, 85, 86, 90]
-pl.plot(x1, y2)
+pl.plot(x1, y2, 'p')
 
 pl.xlabel("Voltaje [V]")
 pl.ylabel("Efficiencia [%]")
