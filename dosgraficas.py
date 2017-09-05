736b0d29b55a5387653e1ff8e4fc94f59993343d
Diana Ivonee Huitzil Sosa <31254618+divoneehs@users.noreply.github.com>
2017-09-05 12:46:52 -0500

    Add files via upload
    Realizamos un grafico de comparación :)

---
+++ dosgraficas.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pylab as pl
+x = [5000, 6000, 7000, 8000, 9000]
+y = [65, 78, 88, 89, 93]
+pl.plot(x, y)
+x1 = [7000, 8000, 9000, 10000, 11000]
+y2 = [65, 75, 85, 86, 90]
+pl.plot(x1, y2)
+
+pl.xlabel("Voltaje [V]")
+pl.ylabel("Efficiencia [%]")
+pl.savefig('comparaciondegraficas.png')
+pl.show()
